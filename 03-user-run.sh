a0525d37839e84a8ef6ef04fe7c4218210212843
dimitrmo <dmouratidis153@gmail.com>
2024-03-01 18:48:21 +0200

    First check if we are using docker-compose

--- 03-user-run.sh
+++ 03-user-run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker compose up --pull always -d --timestamps --remove-orphans
+if command -v docker-compose >/dev/null 2>&1; then
+  docker-compose up --pull always -d --timestamps --remove-orphans
+else
+  docker compose up --pull always -d --timestamps --remove-orphans
+fi
