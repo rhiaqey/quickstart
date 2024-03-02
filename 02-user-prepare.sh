a8e8ba5501bc5bee0bac334215364bbfdcab2539
dimitrmo <dmouratidis153@gmail.com>
2024-03-02 08:06:07 +0200

    Mount redis tmp and logs

--- 02-user-prepare.sh
+++ 02-user-prepare.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+# create directories
 mkdir $(pwd)/data
 mkdir $(pwd)/data/hub
 mkdir $(pwd)/data/redis
+mkdir $(pwd)/data/redis/tmp
+mkdir $(pwd)/data/redis/logs
+mkdir $(pwd)/data/redis/data
 mkdir $(pwd)/data/producers
 mkdir $(pwd)/data/gateways
 mkdir $(pwd)/data/caddy
 
+# assign permissions
 chmod o+w $(pwd)/data
 chmod o+w $(pwd)/data/hub
 chmod o+w $(pwd)/data/redis
+chmod o+w $(pwd)/data/redis/tmp
+chmod o+w $(pwd)/data/redis/logs
+chmod o+w $(pwd)/data/redis/data
 chmod o+w $(pwd)/data/producers
 chmod o+w $(pwd)/data/gateways
 chmod o+w $(pwd)/data/caddy
